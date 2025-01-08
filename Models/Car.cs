25c939c2e0eab41940fe58e27987bd60c908db7a
PC <ohman.mattias@outlook.com>
2025-01-08 13:45:36 +0100

    Add project files.

---
+++ Models/Car.cs
@@ -0,0 +1,11 @@
+﻿namespace FribergCarRentals_GOhman.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public string Year { get; set; }
+        public bool IsAvailable { get; set; }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FribergCarRentals_GOhman.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
